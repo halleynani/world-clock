df445eb6988211dd74595f1b1f1f3ebfb534721c
halleynani <nanihalley@gmail.com>
2025-07-27 16:46:25 -0500

    Added moment, added to js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
-alert("Hello");
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = "1:48:15<small>AM</small>";
